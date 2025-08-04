aec3d7e9a8dfdca531cca87bfe8e8c869430bd8d
Soman Davuluri <somanwordworks@gmail.com>
2025-08-04 13:22:01 +0530

    🔥 Fix Tailwind layout issue for Vercel deployment3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-﻿module.exports = {
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}", // If styles include JSX-based components
   ],
   theme: {
     extend: {},
